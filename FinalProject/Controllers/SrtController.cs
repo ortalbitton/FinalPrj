8176ed3763c373065f7c6f40b55fb25937195171
Owner <Owner@DESKTOP-BEFSNQ2>
2020-11-29 00:23:37 +0200

    Create SrtController and temporary view

---
+++ FinalProject/Controllers/SrtController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.Controllers
+{
+    public class SrtController : Controller
+    {
+        public IActionResult CreateSRT()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public IActionResult CreateSRT()
+        //{
+        //    return View();
+        //}
+    }
+}
--- FinalProject/Models/Srt.cs
+++ FinalProject/Models/Srt.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,12 @@ namespace FinalProject.Models
 {
     public class Srt
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string name { get; set; }
+
+        public string fileId { get; set; }//storage key
     }
 }
