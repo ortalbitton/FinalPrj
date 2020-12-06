5cc237104521c7d45ab0d877bdabffeb0c4e4a37
ortalbitton <ortalbitton3@gmail.com>
2020-12-06 19:08:05 +0200

    fix newComment.js

--- FinalProject/wwwroot/js/newComment.js
+++ FinalProject/wwwroot/js/newComment.js
@@ -1,30 +1,53 @@
 ﻿
 var allReply = document.querySelectorAll('input[id^=reply]');
 
-$('#myform').hide();
+var allNewComments = document.querySelectorAll('div[id^=NewComment]');
+
+var numberComment = 0;
 
 for (var i = 0; i < allReply.length; i++) {
 
     allReply[i].addEventListener('click', function () {
 
-        var form = $('#myform')
-        var url = form.attr('action')
+        $('.comments').hide();
 
-        $('#myform').show();
+        var f = document.createElement("form");
+        f.setAttribute('action', "../Comment/NewComment");
+
+        var form = f
+        var url = form.action;
 
         var PostId = $(this).attr("PostId")
         var pageNumber = parseInt($(this).attr("pageNumber").toString())
+        var numberbtn = $(this).attr("numberbtn")
+
+        allNewComments[parseInt(numberbtn)].classList.add('open');
 
         $.ajax({
             url: url + "?PostId=" + PostId + "&pageNumber=" + pageNumber,
             method: "get",
             success: function (respon) {
-                $("#NewComment").html(respon)
+                allNewComments[numberbtn].innerHTML = respon;
             }
+
         });
 
+        numberComment = numberbtn;
     });
 }
+
+// hide view NewComment
+$(document).mouseup(function (e) {
+    var a = allNewComments[parseInt(numberComment)];
+    if (!a.contains(e.target)) {
+        allNewComments[parseInt(numberComment)].classList.remove('open');
+        $('.comments').show();
+   }
+});
+
+
+
+
 
 
 
